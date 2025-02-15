9b46f0673c149a6b7b71af457f00e4a7248e0bea
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2025-02-15 13:05:29 +0100

    Deploy Governor and MyToken contracts and delegate tokens to self

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,14 @@
+require('dotenv').config()
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-ethers")
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
 };
